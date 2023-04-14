c1d4b5eb621558dc41588e5bc726cf619ea96d82
rajrana437 <rajrana437@gmail.com>
2023-04-14 09:00:01 -0400

    Fix: map marker issue

--- app/src/main/java/com/example/projec_2_raj_rana/MapsFragment.kt
+++ app/src/main/java/com/example/projec_2_raj_rana/MapsFragment.kt
@@ -2,96 +2,149 @@ package com.example.projec_2_raj_rana
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.content.res.Resources
 import android.os.Bundle
-import androidx.core.app.ActivityCompat
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import android.util.Log
-import androidx.annotation.CallSuper
+import com.google.android.gms.maps.model.*
+import java.util.*
+import com.example.projec_2_raj_rana.databinding.FragmentMapsBinding
 
 class MapFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var map: GoogleMap
+    private var _binding: FragmentMapsBinding? = null
+    private val binding get() = _binding!!
+    private val REQUEST_LOCATION_PERMISSION = 1
+    private val TAG = MapFragment::class.java.simpleName
 
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var mMap: GoogleMap
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMapsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
-
-        val mapFragment = childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+    private fun setMapLongClick(map: GoogleMap) {
+        map.setOnMapLongClickListener { latLng ->
+            val snippet = String.format(
+                Locale.getDefault(),
+                "Lat: %1$.5f, Long: %2$.5f",
+                latLng.latitude,
+                latLng.longitude
+            )
+            map.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title(getString(R.string.dropped_pin))
+                    .snippet(snippet)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+            )
+        }
+    }
 
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
+    private fun setPoiClick(map: GoogleMap) {
+        map.setOnPoiClickListener { poi ->
+            val poiMarker = map.addMarker(
+                MarkerOptions()
+                    .position(poi.latLng)
+                    .title(poi.name)
+            )
+            poiMarker?.showInfoWindow()
+        }
+    }
+
+    private fun enableMyLocation(){
+        if (isPermissionGranted()){
+            try {
+                map.isMyLocationEnabled = true
+            } catch (e: SecurityException){
+                Log.e(TAG, "Location permission not granted", e)
+            }
+        }
+        else {
             ActivityCompat.requestPermissions(
                 requireActivity(),
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
-                LOCATION_PERMISSION_REQUEST_CODE
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
             )
-        } else {
-            mMap.isMyLocationEnabled = true
-            getLastKnownLocation()
         }
     }
 
-    private fun getLastKnownLocation() {
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            return
-        }
-
-        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
-            if (location != null) {
-                val currentLatLng = LatLng(location.latitude, location.longitude)
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
-            }
-        }
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        val zoomLevel = 15f
+        val london = LatLng(42.9849233, -81.245276) //London
+        map.addMarker(MarkerOptions().position(london).title("Marker in London"))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(london,zoomLevel))
+        setMapLongClick(map) // Call setMapLongClick() function
+        setPoiClick(map) // Call setPoiClick() function
+        val overlaySize = 100f
+        val androidOverlay = GroundOverlayOptions()
+            .image(BitmapDescriptorFactory.fromResource((R.drawable.android)))
+            .position(london, overlaySize)
+        map.addGroundOverlay(androidOverlay)
+        enableMyLocation()
+        setMapStyle(map)
     }
 
-    @CallSuper
+
+    // Your other functions
+
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
-        permissions: Array<String>,
+        permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getLastKnownLocation()
+        if (requestCode == REQUEST_LOCATION_PERMISSION){
+            if (grantResults.contains(PackageManager.PERMISSION_GRANTED)){
+                enableMyLocation()
             }
         }
     }
+    
+    private fun setMapStyle(map: GoogleMap) {
+        try {
+            val success = map.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    requireContext(),
+                    R.raw.map_style
+                )
+            )
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e(TAG, "Can't find style. Error", e)
+        }
+    }
 
-    companion object {
-        @JvmStatic
-        fun newInstance() = MapFragment()
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
